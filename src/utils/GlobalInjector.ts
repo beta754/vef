a055ae3d387368832e039b0551eb07f24204ca0f
beta <foamier_capstan_0m@icloud.com>
2025-06-28 23:13:02 -0500

    Adds ownerstore

--- src/utils/GlobalInjector.ts
+++ src/utils/GlobalInjector.ts
@@ -1,8 +1,12 @@
+import { EntityOwner } from "../EntityOwner";
+import { ServerEntityStore } from "../ServerEntityStore";
 import { mkSmartRegistry } from "./SmartRegistry";
 
 declare global {
   type InjectedServiceType = keyof InjectedServices
-  interface InjectedServices { }
+  interface InjectedServices {
+    "OwnerStore": ServerEntityStore<EntityOwner>
+  }
 }
 
 export const Injector = mkSmartRegistry<InjectedServices>();
